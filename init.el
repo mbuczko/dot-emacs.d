6fdab4ca0cf8c46a9c157d78e6d1b267c1e5db55
Michal Buczko <michal.buczko@gmail.com>
2015-09-13 20:16:20 +0200

    reducing emacs startup time

--- init.el
+++ init.el
@@ -33,8 +33,8 @@
 
 ;;; Code:
 (defvar current-user
-      (getenv
-       (if (equal system-type 'windows-nt) "USERNAME" "USER")))
+  (getenv
+   (if (equal system-type 'windows-nt) "USERNAME" "USER")))
 
 (message "Prelude is powering up... Be patient, Master %s!" current-user)
 
@@ -43,6 +43,9 @@
 
 ;; Always load newest byte code
 (setq load-prefer-newer t)
+
+;; Optimize GC
+(setq gc-cons-threshold (* 100 1024 1024))
 
 (defvar prelude-dir (file-name-directory load-file-name)
   "The root dir of the Emacs Prelude distribution.")
@@ -69,13 +72,13 @@ by Prelude.")
   (make-directory prelude-savefile-dir))
 
 (defun prelude-add-subfolders-to-load-path (parent-dir)
- "Add all level PARENT-DIR subdirs to the `load-path'."
- (dolist (f (directory-files parent-dir))
-   (let ((name (expand-file-name f parent-dir)))
-     (when (and (file-directory-p name)
-                (not (string-prefix-p "." f)))
-       (add-to-list 'load-path name)
-       (prelude-add-subfolders-to-load-path name)))))
+  "Add all level PARENT-DIR subdirs to the `load-path'."
+  (dolist (f (directory-files parent-dir))
+	(let ((name (expand-file-name f parent-dir)))
+	  (when (and (file-directory-p name)
+				 (not (string-prefix-p "." f)))
+		(add-to-list 'load-path name)
+		(prelude-add-subfolders-to-load-path name)))))
 
 ;; add Prelude's directories to Emacs's `load-path'
 (add-to-list 'load-path prelude-core-dir)
@@ -97,38 +100,40 @@ by Prelude.")
 
 (message "Loading Prelude's core...")
 
-;; the core stuff
-(require 'prelude-packages)
-(require 'prelude-custom)  ;; Needs to be loaded before core, editor and ui
-(require 'prelude-ui)
-(require 'prelude-core)
-(require 'prelude-mode)
-(require 'prelude-company)
-(require 'prelude-editor)
-(require 'prelude-global-keybindings)
+(let ((file-name-handler-alist nil))
+  
+  ;; the core stuff
+  (require 'prelude-packages)
+  (require 'prelude-custom)  ;; Needs to be loaded before core, editor and ui
+  (require 'prelude-ui)
+  (require 'prelude-core)
+  (require 'prelude-mode)
+  (require 'prelude-company)
+  (require 'prelude-editor)
+  (require 'prelude-global-keybindings)
 
-;; OSX specific settings
-(when (eq system-type 'darwin)
-  (require 'prelude-osx))
+  ;; OSX specific settings
+  (when (eq system-type 'darwin)
+	(require 'prelude-osx))
 
-(message "Loading Prelude's modules...")
+  (message "Loading Prelude's modules...")
 
-;; the modules
-(when (file-exists-p prelude-modules-file)
-  (load prelude-modules-file))
+  ;; the modules
+  (when (file-exists-p prelude-modules-file)
+	(load prelude-modules-file))
 
-;; config changes made through the customize UI will be store here
-(setq custom-file (expand-file-name "custom.el" prelude-personal-dir))
+  ;; config changes made through the customize UI will be store here
+  (setq custom-file (expand-file-name "custom.el" prelude-personal-dir))
 
-;; load the personal settings (this includes `custom-file')
-(when (file-exists-p prelude-personal-dir)
-  (message "Loading personal configuration files in %s..." prelude-personal-dir)
-  (mapc 'load (directory-files prelude-personal-dir 't "^[^#].*el$")))
+  ;; load the personal settings (this includes `custom-file')
+  (when (file-exists-p prelude-personal-dir)
+	(message "Loading personal configuration files in %s..." prelude-personal-dir)
+	(mapc 'load (directory-files prelude-personal-dir 't "^[^#].*el$")))
 
-(message "Prelude is ready to do thy bidding, Master %s!" current-user)
+  (message "Prelude is ready to do thy bidding, Master %s!" current-user)
 
-(prelude-eval-after-init
- ;; greet the use with some useful tip
- (run-at-time 5 nil 'prelude-tip-of-the-day))
+  (prelude-eval-after-init
+   ;; greet the use with some useful tip
+   (run-at-time 5 nil 'prelude-tip-of-the-day)))
 
 ;;; init.el ends here
